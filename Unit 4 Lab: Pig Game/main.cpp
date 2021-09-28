b5bad229d2f8d7a000c0db93be6544a1ae1b3061
Coleton Watt <electricninja102@gmail.com>
2021-09-28 07:41:25 -0600

    Initial Commit

---
+++ Unit 4 Lab: Pig Game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unit 4 Lab: Pig Game
+//
+//  Created by Coleton Watt on 9/28/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
